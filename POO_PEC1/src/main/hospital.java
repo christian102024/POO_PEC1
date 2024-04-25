51daa3bd354fa1d03518a91e3f799ad46673783a
Christian <210358@iessierradeguara.com>
2024-04-25 17:59:21 +0200

    UPDATED mostrar menu

--- POO_PEC1/src/main/hospital.java
+++ POO_PEC1/src/main/hospital.java
@@ -12,8 +12,16 @@ public class hospital {
 	public static void main(String[] args) {
 		
 		ArrayList<String> opcionesMenu = new ArrayList<String>(Arrays.asList("Dar de alta a empleado"));
-		MostrarMenu.mostrarMenu(opcionesMenu, "MENU ADMINISTRACION");
+		MostrarMenu.mostrarMenu("MENU ADMINISTRACION", opcionesMenu);
 		Empleados empleados = new Empleados();
 		empleados.darDeAltaEmpleado();
+		
+		ArrayList<String> listaMuyLarga = new ArrayList<String>();
+		
+		for(int i = 0; i < 101; i++) {
+			listaMuyLarga.add(String.valueOf(i) + "textoooooooooooooooooooopppppppppppppppppppp");
+		}
+		
+		MostrarMenu.mostrarMenu("LISTA", listaMuyLarga);
 	}
 }
--- POO_PEC1/src/ui/MostrarMenu.java
+++ POO_PEC1/src/ui/MostrarMenu.java
@@ -9,30 +9,55 @@ public class MostrarMenu {
 
 	}
 	
-	public static void mostrarMenu(ArrayList<String> listaDeOpciones, String titulo) {
+    public static void mostrarMenu(String titulo, ArrayList<String> listaDeOpciones) {
+        int anchoMenu = calcularAnchoMenu(titulo, listaDeOpciones);
+        System.out.println("anchoMenu: " + anchoMenu);
 
-	        // Imprimir bordes superiores del menú
-		System.out.println("+--------------------------+");
-        System.out.println("|      " + titulo + "      |");
-        System.out.println("+--------------------------+");
+        // Imprimir bordes superiores del menú
+        imprimirLinea(anchoMenu);
+        System.out.println("| " + centrarTexto(titulo, anchoMenu - 4) + " |");
+        imprimirLinea(anchoMenu);
 
-	        // Usar forEach para iterar sobre los elementos del ArrayList
-	        int indice = 1;
-	        for (String opcion : listaDeOpciones) {
-	            System.out.println("║ " + indice + ". " + opcion + espacios(20 - opcion.length()) + "║");
-	            indice++;
-	        }
+        // Usar forEach para iterar sobre los elementos del ArrayList
+        int indice = 1;
+        for (String opcion : listaDeOpciones) {
+            System.out.println("| " + indice + ". " + centrarTexto(opcion, anchoMenu - (String.valueOf(indice).length() + 6)) + " |");
+            indice++;
+        }
 
-	        // Imprimir borde inferior del menú
-	        System.out.println("+--------------------------+");
-	}
-	
-	 public static String espacios(int n) {
-	        StringBuilder sb = new StringBuilder();
-	        for (int i = 0; i < n; i++) {
-	            sb.append(" ");
-	        }
-	        return sb.toString();
-	    }
+        // Imprimir borde inferior del menú
+        imprimirLinea(anchoMenu);
+    }
+
+    public static int calcularAnchoMenu(String titulo, ArrayList<String> listaDeOpciones) {
+        int maxLongitud = titulo.length();
+        for (int i = 0; i < listaDeOpciones.size(); i++) {
+        	String opcion = listaDeOpciones.get(i);
+            if (opcion.length() > maxLongitud) {
+            	System.out.println("opcion lenght: " + opcion.length());
+                maxLongitud = opcion.length() + String.valueOf(i).length() + 5;
+            }
+        }
+        return maxLongitud + 10;
+    }
+
+    public static String centrarTexto(String texto, int ancho) {
+        int espaciosTotales = ancho - texto.length();
+        int espaciosIzquierda = espaciosTotales / 2;
+        int espaciosDerecha = espaciosTotales - espaciosIzquierda;
+        return repetirCaracter(' ', espaciosIzquierda) + texto + repetirCaracter(' ', espaciosDerecha);
+    }
+
+    public static String repetirCaracter(char caracter, int veces) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < veces; i++) {
+            sb.append(caracter);
+        }
+        return sb.toString();
+    }
+
+    public static void imprimirLinea(int longitud) {
+        System.out.println("+" + repetirCaracter('-', longitud - 2) + "+");
+    }
 
 }
